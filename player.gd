a502940c6bba67c67e35b030553b9f506a945b48
sirjac86 <sirjac86@kingshigh.school.nz>
2025-06-25 11:49:11 +1200

    re shaped my objects and platforms

--- player.gd
+++ player.gd
@@ -159,3 +159,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_portel_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
+
+
+func _on_killbox_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
--- portel.gd
+++ portel.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-
+@export var level_to_switch_to : PackedScene
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+		get_tree().change_scene_to_packed(level_to_switch_to)
